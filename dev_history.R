e0cadea7b109bceea516b42d75b2208760f5a452
ptaconet <paul.taconet@ird.fr>
2020-03-10 21:43:27 +0000

    change folder structure

--- dev_history.R
+++ dev_history.R
@@ -7,13 +7,20 @@ usethis::use_build_ignore("extdata/")
 usethis::use_build_ignore("figures/")
 usethis::use_build_ignore("r_scripts_database/")
 usethis::use_build_ignore("docs/")
+usethis::use_build_ignore("formation_teledec/")
+usethis::use_git_ignore("formation_teledec/data2")
+usethis::use_git_ignore("formation_teledec/images")
+usethis::use_git_ignore("formation_teledec/formation_teledec.pdf")
 
-
-
+## Run to render the website
 file.copy("README.Rmd","index.Rmd",overwrite=T)
 tx  <- readLines("index.Rmd")
 tx2  <- gsub(pattern = "#  R scripts to model the risk of residual malaria transmission at a micro-scale", replace = "", x = tx)
 tx2  <- gsub(pattern = "output: github_document", replace = "title: \"R scripts to model the risk of residual malaria transmission at a micro-scale\"", x = tx2)
 writeLines(tx2, con="index.Rmd")
+
+# transform R scripts to RM markdown docs
+rmarkdown::render("r_scripts/link_to_r_script.R",output_dir="/home/ptaconet/phd_scripts")
+rmarkdown::render("r_scripts/link_to_r_script_2.R",output_dir="/home/ptaconet/phd_scripts")
 
 rmarkdown::render_site()
